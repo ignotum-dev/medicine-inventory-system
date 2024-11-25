8eaf3381241f663044fb84e55314ff20ad90c20f
giflores1026 <flores.geraldivan@gmail.com>
2024-11-25 14:42:58 +0800

    Table migration

---
+++ database/migrations/2014_10_11_000000_create_medicines_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Models\Category;
+use App\Models\Supplier;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medicines', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand_name');
+            $table->string('generic_name');
+            $table->string('dosage');
+            
+            $table->foreignIdFor(Category::class)
+                ->constrained()
+                ->onDelete('cascade');
+
+            $table->foreignIdFor(Supplier::class)
+                ->constrained()
+                ->onDelete('cascade');
+            
+            $table->string('manufacturer', 255);
+            $table->string('batch_number', 50);
+            $table->date('expiration_date');
+
+            $table->smallInteger('quantity');
+            $table->decimal('purchase_price', 10,2);
+            $table->decimal('selling_price', 10, 2);
+
+            $table->string('description', 255)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medicines');
+    }
+};
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Models\Role;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignIdFor(Role::class)
+                ->constrained()
+                ->onDelete('cascade');
+            
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+
+            $table->date('dob');
+            $table->unsignedTinyInteger('age');
+            $table->enum('sex', ['Male', 'Female']);
+
+            $table->string('address', 255);
+
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
