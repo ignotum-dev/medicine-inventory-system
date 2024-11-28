028ea4a3bcc1866c9d041f5e793705d190d479b0
giflores1026 <flores.geraldivan@gmail.com>
2024-11-28 15:45:27 +0800

    Create a purchase table

---
+++ database/migrations/2024_11_28_105132_create_purchases_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Medicine;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Medicine::class)
+                ->constrained()
+                ->onDelete('cascade');
+            $table->integer('quantity');
+            $table->integer('stocks_left');
+            $table->decimal('selling_price', 10, 2);
+            $table->decimal('total_price', 10, 2);
+            $table->timestamp('purchased_at')->default(now());
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchases');
+    }
+};
