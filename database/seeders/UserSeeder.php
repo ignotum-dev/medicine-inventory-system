07d6b08a3f6792b49454e9d55397fea9e241bc94
ignotum-dev <flores.geraldivan@gmail.com>
2025-01-23 10:23:58 +0800

    Update user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use App\Models\User;
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
@@ -13,6 +16,86 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->count(20)->create();
+        $dob = fake()->dateTimeBetween('-60 years', '-18 years');
+        
+        $data = [
+            [
+                'role_id' => 1,
+                'first_name' => fake()->firstName,
+                'middle_name' => fake()->optional()->firstName,
+                'last_name' => fake()->lastName,
+                'email' => fake()->unique()->safeEmail(),
+                'email_verified_at' => now(),
+                'username' => 'admin',
+                'password' => '',
+                'dob' => $dob,
+                'age' => $this->calculateAge($dob),
+                'sex' => fake()->randomElement(['Male', 'Female']),
+                'address' => fake()->address,
+            ],
+
+            [
+                'role_id' => 2,
+                'first_name' => fake()->firstName,
+                'middle_name' => fake()->optional()->firstName,
+                'last_name' => fake()->lastName,
+                'email' => fake()->unique()->safeEmail(),
+                'email_verified_at' => now(),
+                'username' => 'encoder',
+                'password' => '',
+                'dob' => $dob,
+                'age' => $this->calculateAge($dob),
+                'sex' => fake()->randomElement(['Male', 'Female']),
+                'address' => fake()->address,
+            ],
+
+            [
+                'role_id' => 3,
+                'first_name' => fake()->firstName,
+                'middle_name' => fake()->optional()->firstName,
+                'last_name' => fake()->lastName,
+                'email' => fake()->unique()->safeEmail(),
+                'email_verified_at' => now(),
+                'username' => 'pharmacist',
+                'password' => '',
+                'dob' => $dob,
+                'age' => $this->calculateAge($dob),
+                'sex' => fake()->randomElement(['Male', 'Female']),
+                'address' => fake()->address,
+            ],
+
+            [
+                'role_id' => 4,
+                'first_name' => fake()->firstName,
+                'middle_name' => fake()->optional()->firstName,
+                'last_name' => fake()->lastName,
+                'email' => fake()->unique()->safeEmail(),
+                'email_verified_at' => now(),
+                'username' => 'viewer',
+                'password' => '',
+                'dob' => $dob,
+                'age' => $this->calculateAge($dob),
+                'sex' => fake()->randomElement(['Male', 'Female']),
+                'address' => fake()->address,
+            ],
+            
+
+        ];
+
+        $data = array_map(function ($user) {
+            if (app()->environment('production')) {
+                $user['password'] = bcrypt('P455w0rd');
+            } else {
+                $user['password'] = bcrypt($user['username']);
+            }
+            return $user;
+        }, $data);
+
+        DB::table('users')->insert($data);
+    }
+
+    private function calculateAge($dob)
+    {
+        return Carbon::parse($dob)->age;
     }
 }
