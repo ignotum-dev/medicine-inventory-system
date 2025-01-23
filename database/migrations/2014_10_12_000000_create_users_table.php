eed321d671ceb4490ef75e527f9f4b3700b26697
ignotum-dev <flores.geraldivan@gmail.com>
2025-01-23 10:24:21 +0800

    Add username column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,8 @@ return new class extends Migration
             $table->string('middle_name')->nullable();
             $table->string('last_name');
 
+            $table->string('username')->unique();
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
