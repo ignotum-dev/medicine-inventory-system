556528ee974775a8567f50fe0715e0ae0cd7652a
ignotum-dev <flores.geraldivan@gmail.com>
2025-01-27 15:56:35 +0800

    Update routes

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\MedicineController;
 use App\Http\Controllers\PurchaseController;
 use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\PermissionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,9 +22,9 @@ use App\Http\Controllers\SupplierController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::post('/login', [AuthController::class,'login']);
 
@@ -30,7 +32,7 @@ Route::post('/logout', [AuthController::class,'logout'])
 ->middleware('auth:sanctum');
 
 Route::prefix('/')->group(function () {
-    Route::apiResource('users', UserController::class);
+    // Route::apiresource('users', UserController::class);
     Route::apiResource('categories', CategoryController::class);
     Route::apiResource('suppliers', SupplierController::class);
     Route::apiResource('medicines', MedicineController::class);
@@ -39,4 +41,20 @@ Route::prefix('/')->group(function () {
 Route::prefix('/medicines')->group(function () {
     Route::post('/purchase', [PurchaseController::class, 'purchase']);
     Route::get('/', [MedicineController::class, 'search']);
+})->middleware('auth');
+
+Route::middleware(['auth'])->group(function () {
+    // Permission routes
+    Route::apiResource('permissions', PermissionController::class)->except('destroy');
+    Route::delete('permissions/{permission}', [PermissionController::class, 'destroy'])->name('permissions.destroy');
+
+    // Role routes
+    Route::apiResource('roles', RoleController::class);
+    // Route::get('roles/{role}/add-permissions', RoleController::class, 'addPermissionToRole');
+    // Route::put('roles/{role}/give-permissions', RoleController::class, 'givePermissionToRole');
+    Route::put('roles/{role}/permissions', [RoleController::class, 'addPermissionsToRole'])->name('roles.permissions.add');
+    Route::get('roles/{role}/permissions', [RoleController::class, 'showPermissionsToRole'])->name('roles.permissions.show');
+
+    // User routes
+    Route::apiResource('users', UserController::class);
 });
